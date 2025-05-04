8638908e27619cd6a4232cb5a489efeab92391ab
Victor rodrigues <vitorr.29@hotmail.com>
2025-05-04 00:08:26 -0300

    fix: database connection

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,16 +1,14 @@
-import { config } from 'dotenv';
 import { DataSource } from 'typeorm';
-config();
 
-const isProduction = process.env.PRODUCTION === '1';
+const isProduction = true;
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'trolley.proxy.rlwy.net',
+  port: 48172,
+  username: 'postgres',
+  password: 'PcfETFOMKYtJwOWFRcLdjzuVnyzbPAvB',
+  database: 'railway',
   entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
   synchronize: true,
